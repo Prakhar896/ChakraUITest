7c0dda6a1a830507204bafbeaacb5259708b803b
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-11-01 13:06:11 +0900

    setup complete but unable to run

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Box, Heading } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Box>
       <Heading>Hi!</Heading>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from './src/components/ui'
+import { Provider } from './src/components/ui/provider'
 import './index.css'
 import App from './App.jsx'
 
